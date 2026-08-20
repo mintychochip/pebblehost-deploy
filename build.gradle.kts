4392380e85987a73852492476be7f13d16d37329
mintychochip <mintychochip@users.noreply.github.com>
2026-08-20 01:34:14 -0700

    chore: scaffold Gradle plugin project with wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    `java-gradle-plugin`
+}
+
+group = "dev.pebblehost"
+version = "0.1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.11.0")
+    testImplementation(gradleTestKit())
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+gradlePlugin {
+    plugins {
+        create("pebblehostDeploy") {
+            id = "dev.pebblehost.deploy"
+            implementationClass = "dev.pebblehost.deploy.PebbleHostPlugin"
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
